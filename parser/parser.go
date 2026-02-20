b99c0b1981e2a137cebf1b2d362475678b3aa1a1
saikrishna1908 <saikrishna1908@gmail.com>
2026-02-20 00:30:10 -0600

    add parsing of prefix expressions

--- parser/parser.go
+++ parser/parser.go
@@ -40,6 +40,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
 	return p
 }
@@ -126,6 +128,7 @@ type (
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixPraseFnError(p.curToken.Type)
 		return nil
 	}
 
@@ -150,4 +153,15 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 	lit.Value = value
 
 	return lit
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+
+	p.nextToken()
+	expression.Right = p.parseExpression(PREFIX)
+	return expression
 }
--- parser/parser_helper.go
+++ parser/parser_helper.go
@@ -41,3 +41,10 @@ func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
 func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 	p.infixPraseFns[tokenType] = fn
 }
+
+// Note: this error is thrown if we encounter a prefix with no function mapped to it.
+func (p *Parser) noPrefixPraseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+
+	p.errors = append(p.errors, msg)
+}
